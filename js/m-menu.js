3f39fc1f66508506f982b2f5c128a50b359a4555
maslovkt <63920502+maslovkt@users.noreply.github.com>
2020-10-07 00:17:04 +0700

    Add files via upload

---
+++ js/m-menu.js
@@ -0,0 +1,13 @@
+$(function() {
+    var link = $('.m-menu-link');
+    var close = $('.close');
+    var menu = $('.m-menu');
+        link.on('click', function(event) {
+            event.preventDefault();
+            menu.toggleClass('m-menu_active');
+        }); 
+        close.on('click', function(event) {
+            event.preventDefault();
+            menu.toggleClass('m-menu_active');
+        }); 
+    });
